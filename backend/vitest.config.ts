7e2ef9f6d0371455ed7442418cba298198686681
Chester Chu <chesterchu.cs12@nycu.edu.tw>
2024-05-29 20:54:23 +0800

    CI: test failed case

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
       reporter: ['text', 'html', 'json'], // list of reporters
       reportsDirectory: './coverage', // target directory for coverage reports
       exclude: [
-        'coverage-compare/**' // Exclude specific file
         // You can add more patterns to exclude other files or directories
       ]
     }
